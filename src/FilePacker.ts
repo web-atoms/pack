07d60e7c2c6f502cf86130ed6be872a426bccc72
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-01-23 19:56:47 +0530

    fixed umd inject

--- src/FilePacker.ts
+++ src/FilePacker.ts
@@ -38,16 +38,16 @@ export default class FilePacker {
         }
 
         const outputFile = this.file + ".pack.js";
-        await fileApi.writeString(outputFile, `
+
+        const umd = await fileApi.readString(`${this.root}/node_modules/web-atoms-amd-loader/umd.js`);
+
+        await fileApi.writeString(outputFile, "");
+        await fileApi.appendString(outputFile, umd + "\r\n");
+        await fileApi.appendString(outputFile, `
         AmdLoader.instance.register(
             [${ packages.map((s) => JSON.stringify(s)).join(",") }],
             [${ this.header.map((s) => JSON.stringify(s)).join(",") }]);
 `);
-
-        const umd = await fileApi.readString(`${this.root}/node_modules/web-atoms-amd-loader/umd.js`);
-
-        await fileApi.appendString(outputFile, umd + "\r\n");
-
         for (const iterator of this.content) {
             await fileApi.appendString(outputFile, iterator + "\r\n");
         }

43f5a9607d5449b95a93ebc7c234f6371b6cb9c7
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-01-08 20:57:13 +0530

    Added media filter

--- src/FilePacker.ts
+++ src/FilePacker.ts
@@ -189,6 +189,7 @@ export default class FilePacker {
 
             const ds = dependencies
                 .map((s) => s.startsWith(".") ? DeclarationParser.resolveRelativePath(s, name) : s)
+                .filter((s) => !/\.(jpg|jpeg|svg|png|json|html|mp4|mp3|gif)$/i.test(s))
                 .map((s) => DeclarationParser.parsePackage(s))
                 .map((s) =>
                     ({

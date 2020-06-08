5c061e3772cb9519076919abc41615f4b3ef979e
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-06-08 13:54:24 +0530

    wip

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ var Packer = require("./dist/FilePacker").default;
 exports.default = function (cb, p) {
     var dir = p[0];
     var file = p[1];
-    var pkg = JSON.parse(p[2]);
+    var pkg = p[2];
     var packer = new Packer(dir, file, pkg);
     packer.pack()
         .then(function() {

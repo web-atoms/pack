127cd121cab9b85e545bfc05e9b8e3b2de3b2e0e
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-06-08 11:20:01 +0530

    wip

---
+++ run.js
@@ -0,0 +1,12 @@
+var Packer = require("./dist/Packer").default;
+
+var packer = new Packer();
+exports.default = function (cb, p) {
+    packer.run(p)
+        .then(function() {
+            cb(null,"done");
+        })
+        .catch(function(e) {
+            cb(e);
+        });
+}
